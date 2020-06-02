5a6f449388abcec22d4c9aa9f28a45688a171650
uruwanara <uruwanara@users.noreply.github.com>
2020-06-02 20:04:30 +0530

    Update KMPStringSearch.java

--- KMPStringSearch/src/kmpstringsearch/KMPStringSearch.java
+++ KMPStringSearch/src/kmpstringsearch/KMPStringSearch.java
@@ -11,41 +11,31 @@ public class KMPStringSearch {
 
     static int count = 0;
 
-    void KMPSearch(String bDay, Vector<Character> vect) {
+    void KMP(String bDay, Vector<Character> vect) {
         try {
             BufferedWriter writter = new BufferedWriter(new FileWriter("results.txt", true));
-             writter.append("====================================================================================\n\t\t KMP String Search Method Results"
-                    + "\n====================================================================================\n");
+            writter.append("====================================================================================\n\t\t KMP String Search Method Results"
+                    + "\n====================================================================================\n");     // create a results.txt file if doesnt exist and update it
 
+            int LPS[] = new int[6];
+            int j = 0;
 
-            // create a results.txt file if doesnt exist and update it
-            // create lps[] that will hold the longest 
-            // prefix suffix values for pattern 
-            int lps[] = new int[6];
-            int j = 0; // index for pat[] 
+            findLPS(bDay, LPS); // pass the bday string to calculate the LPS
 
-            // Preprocess the pattern (calculate lps[] 
-            // array) 
-            computeLPSArray(bDay, 6, lps);
-
-            int i = 0; // index for txt[] 
+            int i = 0;
             while (i < vect.size()) {
                 if (bDay.charAt(j) == vect.get(i)) {
                     j++;
                     i++;
                 }
                 if (j == 6) {
-                    System.out.println("BirthDay Found At : " + (i - j));
-                    writter.append("dsndnisndis");
+                    // System.out.println("BirthDay Found At : " + (i - j));
                     writter.append("BirthDay Found At : " + (i - j) + "\n");
                     count++;
-                    j = lps[j - 1];
-                } // mismatch after j matches 
-                else if (i < vect.size() && bDay.charAt(j) != vect.get(i)) {
-                    // Do not match lps[0..lps[j-1]] characters, 
-                    // they will match anyway 
+                    j = LPS[j - 1];
+                } else if (i < vect.size() && bDay.charAt(j) != vect.get(i)) {
                     if (j != 0) {
-                        j = lps[j - 1];
+                        j = LPS[j - 1];
                     } else {
                         i = i + 1;
                     }
@@ -54,42 +44,32 @@ public class KMPStringSearch {
             writter.close();
         } catch (Exception e) {
             System.out.println(e);
-
         }
     }
 
-    void computeLPSArray(String bDay, int M, int lps[]) {
-        // length of the previous longest prefix suffix 
+    void findLPS(String bDay, int LPS[]) {
         int len = 0;
         int i = 1;
-        lps[0] = 0; // lps[0] is always 0 
+        LPS[0] = 0;  // we can set oth element of LPS becase it is the first element of the array so there are no repetioins 
 
-        // the loop calculates lps[i] for i = 1 to M-1 
-        while (i < M) {
+        while (i < 6) {
             if (bDay.charAt(i) == bDay.charAt(len)) {
                 len++;
-                lps[i] = len;
+                LPS[i] = len;
                 i++;
-            } else // (pat[i] != pat[len]) 
-            {
-                // This is tricky. Consider the example. 
-                // AAACAAAA and i = 7. The idea is similar 
-                // to search step. 
-                if (len != 0) {
-                    len = lps[len - 1];
+            } else {
 
-                    // Also, note that we do not increment 
-                    // i here 
-                } else // if (len == 0) 
-                {
-                    lps[i] = len;
+                if (len != 0) {
+                    len = LPS[len - 1];
+
+                } else {
+                    LPS[i] = len;
                     i++;
                 }
             }
         }
     }
 
-    // Driver program to test above function 
     public static void main(String[] args) {
         int temp = 0;
         Scanner sc2 = null;
@@ -129,9 +109,7 @@ public class KMPStringSearch {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt", true)); // create a results.txt file if doesnt exist and update it
 
-           
-            String BD = new String(bDay);
-            new KMPStringSearch().KMPSearch(BD, vect);
+            new KMPStringSearch().KMP(new String(bDay), vect); // pass the values to the KMP function
 
             writer.append("Number of all the results found : " + count + "\n");
 
@@ -139,6 +117,7 @@ public class KMPStringSearch {
         } catch (Exception e) {
             System.out.println(e);
         }
+        System.out.println("Successfully Added to results.txt");
     }
 
 }
